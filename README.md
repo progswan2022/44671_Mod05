615c411fb8028b770a40bda9d2d3e6be1e66480c
Erin Swan-Siegel <prog.swan@gmail.com>
2023-09-24 00:50:59 -0500

    Module 05 Upload

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# 44671_Mod05
+# 44671_Mod05 - Creating a Producer
+
+Streaming data may come from web analytics, social media, smart devices, and more. In these next two modules, we'll look at implementing analytics for a "smart smoker" (as in slow cooked food). 
+
+In Module 5, we'll understand the process, design our system, and implement the producer. In Module 6, we'll add the consumers, implementing analytics based on a rolling window of time, and raise an alert when interesting events are detected.  
+
+- Course: 44-671 : Module 05
+- Student: Erin Swan-Siegel
+- Date: 09-22-2023
+
+# Instructions
+1. Fork this starter repo into your GitHub.
+1. Clone your repo down to your machine.
+1. View / Command Palette - then Python: Select Interpreter
+1. Select your conda environment. 
+
+## RabbitMQ Admin 
+
+RabbitMQ comes with an admin panel. When you run the task emitter, reply y to open it. 
+
+(Python makes it easy to open a web page - see the code to learn how.)
+
+## Edit your constants
+The file v3_emitter_of_tasks.py contains the below constants. Review and adjust where required prior to executing the producer.
+- file_name : CSV for the file to be read
+- sleep_secs : the amount of time (in seconds) between each sent message
+- host : define the host name
+- queue_01 : Temperature of Smoker
+- queue_02 : Temperature of Food A
+- queue_03 : Temperature of Food B
+
+## Execute the Producer
+
+1. Run v3_emitter_of_tasks.py (say y to monitor RabbitMQ queues)
+
+## References
+- Assignment: https://nwmissouri.instructure.com/courses/54849/assignments/857700?module_item_id=1893393
+- Prompt: https://nwmissouri.instructure.com/courses/54849/pages/smart-smoker 
+- Guidance 5.1: https://nwmissouri.instructure.com/courses/54849/pages/module-5-dot-1-guided-producer-design?wrap=1 
+- Guidance 5.2: https://nwmissouri.instructure.com/courses/54849/pages/module-5-dot-2-guided-producer-implementation?wrap=1
+
+## Screenshot
+A running example of the emitter and RabbitMQ queue names and respective counters
+![Erin's PC](Module05_screenshot.png)
---
+++ v3_emitter_of_tasks.py
@@ -0,0 +1,129 @@
+"""
+Course: 44-671 : Module 05
+Student: Erin Swan-Siegel
+Date: 09-22-2023
+
+    This program reads rows from a .csv file and sends the value in each field to their respective queues
+    on the RabbitMQ server, representing temperature readings from a smoker and up to two foods.
+    This is Part 1 of 2, with the listeners/consumers being configured in Module 06.
+    
+
+"""
+# Import modules
+import pika
+import sys
+import webbrowser
+import csv
+import socket
+import time
+import logging
+
+# Configuration Constants
+file_name = "smoker-temps.csv"
+sleep_secs = 30
+host = "localhost"
+queue_01 = "01-smoker"
+queue_02 = "02-food-A"
+queue_03 = "03-food-B"
+
+# Function to prompt user to use the RabbitMQ Admin website; runs when show_offer == True
+def offer_rabbitmq_admin_site():
+    """Offer to open the RabbitMQ Admin website"""
+    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+    print()
+    if ans.lower() == "y":
+        webbrowser.open_new("http://localhost:15672/#/queues")
+        print()
+    
+# Function to remove/clear-out queues from RabbitMQ
+def reset_RabbitMQ():
+    # create a blocking connection to the RabbitMQ server
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+    # use the connection to create a communication channel
+    ch = conn.channel()
+    # delete all known channel names
+    ch.queue_delete(queue_01)
+    ch.queue_delete(queue_02)
+    ch.queue_delete(queue_03)
+
+# Function remains unchanged from v2 
+def send_message(queue_name: str, message: str):
+    """
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
+
+    Parameters:
+        host (str): the host name or IP address of the RabbitMQ server
+        queue_name (str): the name of the queue
+        message (str): the message to be sent to the queue
+    """
+    try:
+        # create a blocking connection to the RabbitMQ server
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+        # use the connection to create a communication channel
+        ch = conn.channel()
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
+        # print a message to the console for the user
+        print(f" [x] Sent {message}")
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
+    finally:
+        # close the connection to the server
+        conn.close()
+
+# Define Streaming data process
+# Based on streaming code from Module 01
+# Function accepts user information regarding the file to be read
+def stream_row(file_name):
+    """Read from input file and stream data."""
+    logging.info(f"Starting to stream data from {file_name}")
+
+    # Create a file object for input (r = read access)
+    with open(file_name, "r") as input_file:
+        logging.info(f"Opened for reading: {file_name}.")
+
+        # Create a CSV reader object
+        reader = csv.reader(input_file, delimiter=",")
+        
+        # Skip the header row and continue
+        # We can get user prompt function from Module04
+        header = next(reader)  # Skip header row
+        logging.info(f"Skipped header row: {header}")
+
+        # For each data row in the reader, read and send as a message then pause before reading the next row
+        # using the value.join([array with comma-separated values]) will allow for future parsing
+        for row in reader:
+            # print(row)
+            send_message(queue_01,",".join([row[0],row[1]]))
+            send_message(queue_02,",".join([row[0],row[2]]))
+            send_message(queue_03,",".join([row[0],row[3]]))
+            logging.info(f"Reading row {reader.line_num}.")
+            time.sleep(sleep_secs) # wait 30 seconds between messages
+
+
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__": 
+    # ask the user if they'd like to open the RabbitMQ Admin site
+    # based on the T/F setting of show_offer variable
+    show_offer = True 
+    if show_offer == True:
+        offer_rabbitmq_admin_site()
+    
+    # Call function reset_RabbitMQ to clear out previously-created queues
+    # In the future, this could be based on user input or possibly defining the queues, creating them if they
+    # don't exist and purging them if they do, instead of deleting fully each time
+    reset_RabbitMQ()
+
+    # Stream values from csv file
+    stream_row(file_name)
